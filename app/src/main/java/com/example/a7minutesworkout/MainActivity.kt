59ed509edec270554c03664a1ab58cafde701c6f
omprasad1997 <20rathodomprasad@gmail.com>
2022-06-02 12:06:29 +0530

    Initialize view binding and get rid of action bar

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val fLStartButton: FrameLayout = findViewById(R.id.flStart)
-        fLStartButton.setOnClickListener {
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this,
                 "Here we will start the exercise.",
