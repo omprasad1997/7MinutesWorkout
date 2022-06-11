8f95b39a88b44c027d49968c4e548a52fd6ea27e
omprasad1997 <20rathodomprasad@gmail.com>
2022-06-11 13:31:30 +0530

    read the history from database and showd into the logcat

--- app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -2,8 +2,11 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.a7minutesworkout.databinding.ActivityBmiBinding
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -26,5 +29,19 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        val dao = (application as WorkoutApp).db.historyDao()
+
+        lifecycleScope.launch {
+           dao.fetchAllDates().collect {
+               val list = ArrayList(it)
+               Log.e("fetched data : " , list.toString())
+           }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
